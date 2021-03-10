d2adc8a246a98b19b4a8367ad715c5e537844b67
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-10 20:13:32 +0200

    Luotu pohja tuleville metodeille

--- Prototype/Prototype/Survey.cs
+++ Prototype/Prototype/Survey.cs
@@ -2,3 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+namespace Prototype
+{
+    public class Survey
+    {
+        //private string introMessage;
+        //private emojis = new List<(int emojiID, int impact, string[] activities)>;
+        //private string roomCode;
+
+        public void setIntroMessage(string)
+        {
+            
+        }
+        public getIntroMessage()
+        {
+            return this.introMessage;
+        }
+
+        public void setEmoji(int index, int id)
+        {
+            
+        }
+        public void setEmojiImpact(int index, int value)
+        {
+            
+        }
+        public void addActivity(int index, string value)
+        {
+            
+        }
+        public void removeActivity(int index, string)
+        {
+            
+        }
+        public getEmojis()
+        {
+            return this.emojis;
+        }
+    }
+}
