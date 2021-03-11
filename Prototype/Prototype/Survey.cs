f4ead39912e475bbc34139ac848f2bb5fda63e2d
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-11 13:33:03 +0200

    Added debug lines for methods in Survey.cs

--- Prototype/Prototype/Survey.cs
+++ Prototype/Prototype/Survey.cs
@@ -12,37 +12,40 @@ namespace Prototype
 
         public void setIntroMessage(string)
         {
-            
+            Console.WriteLine("$DEBUG: setting an intro message for the survey");
+            //introMessage = Console.ReadLine();
         }
 
         public string getIntroMessage()
         {
-            return this.introMessage;
+            Console.WriteLine("$DEBUG: getting an intro message from the survey");
+            //return this.introMessage;
         }
 
         public void setEmoji(int index, int id)
         {
-            
+            Console.WriteLine("$DEBUG: setting an emoji for the survey");
         }
 
         public void setEmojiImpact(int index, int value)
         {
-            
+            Console.WriteLine("$DEBUG: setting an impact for emoji");
         }
 
         public void addActivity(int index, string value)
         {
-            
+            Console.WriteLine("$DEBUG: adding activity for emoji");
         }
 
         public void removeActivity(int index, string)
         {
-            
+            Console.WriteLine("$DEBUG: removing activity from certain emoji in the survey");
         }
 
         public List<int, int, List<string>> getEmojis()
         {
-            return this.emojis;
+            Console.WriteLine("$DEBUG: setting emojis (emoji, impact and activities) from the survey");
+            //return this.emojis;
         }
     }
 }
