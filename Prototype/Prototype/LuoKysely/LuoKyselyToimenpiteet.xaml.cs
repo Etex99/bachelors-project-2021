61949a829a74848d21a81ef7b9c65ad8d489c790
tkultala <80053122+tkultala@users.noreply.github.com>
2021-03-11 15:01:31 +0200

    Perus navigointi sivujen välille lisätty: Etusivu ja LuoKysely

---
+++ Prototype/Prototype/LuoKysely/LuoKyselyToimenpiteet.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LuoKyselyToimenpiteet : ContentPage
+    {
+        public LuoKyselyToimenpiteet()
+        {
+            InitializeComponent();
+        }
+        async void JatkaButtonClicked(object sender, EventArgs e)
+        {
+            // siirrytään "Luo kysely -lopetus" sivulle 
+            await Navigation.PushAsync(new LuoKyselyLopetus()); ;
+        }
+    }
+}
