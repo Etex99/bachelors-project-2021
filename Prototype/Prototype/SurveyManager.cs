570c1df55d43a18633e8fb0e43848132466c6637
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-11 20:14:56 +0200

    Little changes for now to the SaveSurvey, LoadSurvey and DeleteSurvey methods by adding name = "test.txt" for testing purposes

--- Prototype/Prototype/SurveyManager.cs
+++ Prototype/Prototype/SurveyManager.cs
@@ -12,22 +12,22 @@ namespace Prototype
         private string[] surveyTemplates;
         private readonly string folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 
-        public bool SaveSurvey(Survey survey, string name)
+        public bool SaveSurvey(Survey survey, string name = "test.txt")
         {
             string jsonString = JsonSerializer.Serialize(survey);
             if(Device.RuntimePlatform == Device.Android)
             {
-                string path = Path.Combine(folder, "test.txt" /*name*/);
+                string path = Path.Combine(folder, name);
                 File.WriteAllText(path, jsonString);
             }
             return true;
         }
 
-        public Survey LoadSurvey(string name)
+        public Survey LoadSurvey(string name = "test.txt")
         {
             if(Device.RuntimePlatform == Device.Android)
             {
-                string path = Path.Combine(folder, "test.txt" /*name*/);
+                string path = Path.Combine(folder, name);
                 string jsontext = File.ReadAllText(path);
                 survey = JsonSerializer.Deserialize<Survey>(jsontext);
             }
@@ -40,9 +40,9 @@ namespace Prototype
             return surveyTemplates;
         }
 
-        public void DeleteSurvey(string name)
+        public void DeleteSurvey(string name = "test.txt")
         {
-            string path = Path.Combine(folder, "test.txt" /*name*/);
+            string path = Path.Combine(folder, name);
             Directory.Delete(path);
         }
 
