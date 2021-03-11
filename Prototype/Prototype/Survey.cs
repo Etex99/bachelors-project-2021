3c7f2ea62430e2abd112195e0c1272e445c796ad
Etex99 <emma99.kem@gmail.com>
2021-03-11 19:42:50 +0200

    Survey class fix

--- Prototype/Prototype/Survey.cs
+++ Prototype/Prototype/Survey.cs
@@ -1,55 +1,34 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Prototype
 {
     public class Survey
     {
-        //private string introMessage;
-        //private List<int, int, List<string>> emojis;
-        //private string roomCode;
+        public string introMessage = "default";
 
-        public void setIntroMessage(string)
-        {
-            Console.WriteLine("$DEBUG: setting an intro message for the survey");
-            //introMessage = Console.ReadLine();
-        }
+        public class Emoji {
+			public int ID = 0;
+            public int Impact = 0;
+			public List<string> activities = null;
 
-        public string getIntroMessage()
-        {
-            Console.WriteLine("$DEBUG: getting an intro message from the survey");
-            //return this.introMessage;
-        }
+            public Emoji() {
+                ID = 0;
+                Impact = 0;
+                activities = new List<string>() { "default", "foo", "bar" };
+			}
 
-        public void setEmoji(int index, int id)
-        {
-            Console.WriteLine("$DEBUG: setting an emoji for the survey");
-            //emojis[index][0] = id;
-        }
+            public Emoji(int ID, int impact, List<string> activities) {
+				this.ID = ID;
+				this.Impact = impact;
+                this.activities = activities;
+			}
+		}
 
-        public void setEmojiImpact(int index, int value)
-        {
-            Console.WriteLine("$DEBUG: setting an impact for emoji");
-            //emojis[index][1] = value;
-        }
+        public List<Emoji> emojis;
+        public string RoomCode = "default";
 
-        public void addActivity(int index, string value)
-        {
-            Console.WriteLine("$DEBUG: adding activity for emoji");
-            //emojis[index][2].Add(value);
-        }
+        public Survey() {
 
-        public void removeActivity(int index, string value)
-        {
-            Console.WriteLine("$DEBUG: removing activity from certain emoji in the survey");
-            //emojis[index][2].Remove(value);
-        }
-
-        public List<int, int, List<string>> getEmojis()
-        {
-            Console.WriteLine("$DEBUG: setting emojis (emoji, impact and activities) from the survey");
-            //return this.emojis;
         }
     }
 }
