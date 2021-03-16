7fbd7fc285cd3682a8a4413aff5b8a7034c63b87
tkultala <tkultala@student.oulu.fi>
2021-03-16 15:02:45 +0200

    Added experimental drop down menu, not connected with back end

--- bachelors-project-2021/Prototype/Prototype/LuoKysely/LuoKyselyJohdatus.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/LuoKysely/LuoKyselyJohdatus.xaml.cs
@@ -12,20 +12,33 @@ namespace Prototype
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LuoKyselyJohdatus : ContentPage
     {
-
         
+
+
+
+
         public LuoKyselyJohdatus()
         {
             InitializeComponent();
+            
 
-            //alustetaan näkymä avatun kyselyn introviestillä
-            editor.Text = SurveyManager.GetInstance().GetSurvey().introMessage;
+            BindingContext = this;
+
         }
+
+                
+                
+
+
+                //alustetaan näkymä avatun kyselyn introviestillä
+                // picker. = SurveyManager.GetInstance().GetSurvey().introMessage;
+        
 
         async void JatkaButtonClicked(object sender, EventArgs e)
         {
             //kyselyn johdatuslause asetetaan.
-            SurveyManager.GetInstance().GetSurvey().introMessage = editor.Text;
+            // SurveyManager.GetInstance().GetSurvey().introMessage = 
+          
 
             //siirrytään "luo uus kysely 2/3" sivulle 
             await Navigation.PushAsync(new LuoKyselyEmojit());
