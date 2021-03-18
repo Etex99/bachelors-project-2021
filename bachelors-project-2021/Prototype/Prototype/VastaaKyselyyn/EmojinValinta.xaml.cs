2068902290cbb083bbe095e32b50d4e0f021c354
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-18 15:05:28 +0200

    Made some progress to the EmojiValinta funtionality

--- bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/EmojinValinta.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/EmojinValinta.xaml.cs
@@ -12,13 +12,25 @@ namespace Prototype
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EmojinValinta : ContentPage
     {
+        public IList<Emoji> Emojis { get; set; }
+        public IList<string> Images { get; set; }
         public EmojinValinta()
         {
             InitializeComponent();
 
-
+            Emojis = SurveyManager.GetInstance().GetSurvey().emojis; //Change to survey provided by host
+            Images = new List<string>();
+            foreach(Emoji emoji in Emojis)
+            {
+                Images.Add(emoji.ImageSource);
+            }
 
             BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine((sender as Button).ClassId.ToString());
         }
     }
 }
