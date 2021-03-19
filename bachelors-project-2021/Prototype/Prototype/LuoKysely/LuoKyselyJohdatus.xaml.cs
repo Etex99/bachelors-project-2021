10d23b755f8193c0e32dbc5750df0851722f4720
tkultala <tkultala@student.oulu.fi>
2021-03-19 14:27:45 +0200

    "Drop down menu" created for intros. Not connected with back end.

--- bachelors-project-2021/Prototype/Prototype/LuoKysely/LuoKyselyJohdatus.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/LuoKysely/LuoKyselyJohdatus.xaml.cs
@@ -12,27 +12,72 @@ namespace Prototype
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LuoKyselyJohdatus : ContentPage
     {
-        
+
+
+        //Testausta varten
+        public IList<CollectionItem> Intros { get; private set; }
+
+        public class CollectionItem
+        {
+            public string introMessage { get; set; }
+
+            public override string ToString()
+            {
+                return introMessage;
+            }
+        }
 
 
 
+      
 
         public LuoKyselyJohdatus()
         {
             InitializeComponent();
-            
+
+            //Testausta varten
+            Intros = new List<CollectionItem>();
+
+            Intros.Add(new CollectionItem
+            {
+                    introMessage="Testi intro1 "
+            });
+
+            Intros.Add(new CollectionItem
+            {
+                introMessage = "Testi intro2 "
+            });
+
+            Intros.Add(new CollectionItem
+            {
+                introMessage = "Testi intro3 "
+            });
 
             BindingContext = this;
 
+
+            //alustetaan näkymä avatun kyselyn introviestillä
+            // = SurveyManager.GetInstance().GetSurvey().introMessage;
         }
 
-                
-                
+        private void btnPopupButton_Clicked(object sender, EventArgs e)
+        {
+            popupSelection.IsVisible = true;
+
+        }
+
+        private void Sulje_Clicked(object sender, EventArgs e)
+        {
+            popupSelection.IsVisible = false;
+        }
 
 
-                //alustetaan näkymä avatun kyselyn introviestillä
-                // picker. = SurveyManager.GetInstance().GetSurvey().introMessage;
-        
+        void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CollectionItem selectedItem = e.CurrentSelection[0] as CollectionItem;
+        }
+
+
 
         async void JatkaButtonClicked(object sender, EventArgs e)
         {
