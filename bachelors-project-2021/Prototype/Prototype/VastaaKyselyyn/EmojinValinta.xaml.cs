787236703166bc087f3ccf2c21a9955952e7ab67
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-22 17:20:36 +0200

    Removed unnecessary namespaces

--- bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/EmojinValinta.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/EmojinValinta.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +12,7 @@ namespace Prototype
         public IList<Emoji> Emojis { get; set; }
         public IList<string> Images { get; set; }
         private string answer;
+
         public EmojinValinta()
         {
             InitializeComponent();
