fc4133f43affca0fb5a5cd3742bfb4f4d5b2b509
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-24 12:41:36 +0200

    Removed the connection to the activity vote

--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenvetoClient.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenvetoClient.xaml.cs
@@ -20,12 +20,5 @@ namespace Prototype
         {
             await Navigation.PushAsync(new MainPage());
         }
-
-
-        //Tämä painike on ihan vaan sitä varten että pääsee näkemään ja testaamaan äänestyksen näytöt
-        async void TestaustaClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new AktiviteettiäänestysEka());
-        }
     }
 }
