067b17dce8e5e9aebd5c9f964e4d7fccdb4785e4
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-26 16:05:21 +0200

    Finally managed to make the emoji selection work as intended :3

--- bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/EmojinValinta.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/EmojinValinta.xaml.cs
@@ -29,7 +29,16 @@ namespace Prototype
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            b0.BorderColor = Color.Transparent;
+            b1.BorderColor = Color.Transparent;
+            b2.BorderColor = Color.Transparent;
+            b3.BorderColor = Color.Transparent;
+            b4.BorderColor = Color.Transparent;
+            b5.BorderColor = Color.Transparent;
+            b6.BorderColor = Color.Transparent;
+
             Button emoji = sender as Button;
+            emoji.BorderColor = Color.Gold;
             answer = emoji.ClassId.ToString();
             Console.WriteLine(answer);
         }
