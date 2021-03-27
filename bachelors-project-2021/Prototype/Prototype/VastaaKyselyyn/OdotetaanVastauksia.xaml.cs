bc25aa94ec6b741ce11c3bbd92b434074ed8732a
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-27 18:05:46 +0200

    OdotetaanVastauksia page screen added which leads to the host results page

---
+++ bachelors-project-2021/Prototype/Prototype/VastaaKyselyyn/OdotetaanVastauksia.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OdotetaanVastauksia : ContentPage
+    {
+        public OdotetaanVastauksia()
+        {
+            InitializeComponent();
+        }
+
+        private void JatkaTuloksiin(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new TabbedViewHost());
+        }
+    }
+}
