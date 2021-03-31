ea83d954695017c965be468cac03f65235c53570
JesseHuttunen <jesse.huttunen@gmail.com>
2021-03-31 12:47:04 +0300

    fixed some typos and added initialization for voteCandidates

--- bachelors-project-2021/Prototype/Prototype/ActivityVote.cs
+++ bachelors-project-2021/Prototype/Prototype/ActivityVote.cs
@@ -6,22 +6,23 @@ namespace Prototype
 {
     class ActivityVote
     {
-        private Dictionary<int, string[]> vote1Cadidates;
-        private List<string> vote2Cadidates;
+        private Dictionary<int, string[]> vote1Candidates;
+        private List<string> vote2Candidates;
 
-        public ActivityVote (List<Emoji> emojis, Dictionary<int, int> emojiResults)
+        public ActivityVote ()
         {
-            
+            vote1Candidates = new Dictionary<int, string[]>();
+            vote2Candidates = new List<string>();
         }
 
         public Dictionary<int, string[]> calcVote1Candidates(List<Emoji> emojis, Dictionary<int, int> emojiResults)
         {
-            return vote1Cadidates;
+            return vote1Candidates;
         }
 
         public List<string> calcVote2Candidates(Dictionary<string, int> vote1Results)
         {
-            return vote2Cadidates;
+            return vote2Candidates;
         }
     }
 }
