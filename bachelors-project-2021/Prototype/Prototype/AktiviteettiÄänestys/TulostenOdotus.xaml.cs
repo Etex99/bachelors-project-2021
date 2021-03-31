33bdaddbc92b3e63bf4b4b884a91dd19facc9540
tkultala <tkultala@student.oulu.fi>
2021-03-31 12:10:37 +0300

    Created screen for host to wait activity vote results

---
+++ bachelors-project-2021/Prototype/Prototype/AktiviteettiÄänestys/TulostenOdotus.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TulostenOdotus : ContentPage
+    {
+        public TulostenOdotus()
+        {
+            InitializeComponent();
+
+            //poistetaan turha navigointipalkki
+            NavigationPage.SetHasNavigationBar(this, false); 
+
+            //Siirrytään aktiviteettin äänestykseen tuloksiin 10skuluttua. (timer testi) 
+            Device.StartTimer(TimeSpan.FromSeconds(10), () =>
+            {   
+                Navigation.PushAsync(new AktiviteettiäänestysTulokset());
+                return false;
+            });
+        }
+    }
+}
