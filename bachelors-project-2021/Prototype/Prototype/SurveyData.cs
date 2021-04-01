044ce6919d9522d3b179907f232875c3d0f7f6e9
JesseHuttunen <jesse.huttunen@gmail.com>
2021-04-01 14:19:25 +0300

    added addvote1results method and also vote1results dictionary variable, temporarily changed tostring override method to test it out.

--- bachelors-project-2021/Prototype/Prototype/SurveyData.cs
+++ bachelors-project-2021/Prototype/Prototype/SurveyData.cs
@@ -7,9 +7,11 @@ namespace Prototype
 	class SurveyData
 	{
 		private Dictionary<int, int> emojiResults;
+		private Dictionary<string, int> vote1Results;
 
 		public SurveyData() {
 			emojiResults = new Dictionary<int, int>();
+			vote1Results = new Dictionary<string, int>();
 		}
 		
 		//Adds a single emoji answer to results
@@ -23,12 +25,38 @@ namespace Prototype
 			//emoji did not exist yet
 			emojiResults.Add(emoji, 1);
 		}
+		public void AddVote1Results(string activity)
+        {
+			int count;
+			if(vote1Results.TryGetValue(activity, out count))
+            {
+				count++;
+				vote1Results[activity] = count;
+				return;
+            }
+			vote1Results.Add(activity, 1);
+        }
 		public Dictionary<int, int> GetEmojiResults() {
 			return emojiResults;
 		}
+		public Dictionary<string, int> GetVote1Results()
+        {
+			return vote1Results;
+        }
 		public override string ToString() {
-			string s = "{emojiResults: ";
+			/*string s = "{emojiResults: ";
 			foreach (var item in emojiResults)
+			{
+				s += "[";
+				s += item.Key;
+				s += ": ";
+				s += item.Value;
+				s += "],";
+			}
+			s = s.Substring(0, s.Length - 1);
+			s += "}";*/
+			string s = "{vote1Results: ";
+			foreach (var item in v)
 			{
 				s += "[";
 				s += item.Key;
