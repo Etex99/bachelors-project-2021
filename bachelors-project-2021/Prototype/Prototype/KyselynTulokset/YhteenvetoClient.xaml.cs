66c16be8ddd49b583bea2086d48e6962200ddb05
JesseHuttunen <jesse.huttunen@gmail.com>
2021-04-02 13:21:31 +0300

    Made the logic how the emojis, scale, on top of that what emojis scale etc.

--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenvetoClient.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenvetoClient.xaml.cs
@@ -12,9 +12,33 @@ namespace Prototype
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class YhteenvetoClient : ContentPage
     {
+        public IList<string> resultImages { get; set; }
+        public IList<double> resultScale { get; set; }
         public YhteenvetoClient()
         {
             InitializeComponent();
+            resultImages = new List<string>();
+            resultScale = new List<double>();
+
+            int count = 0;
+            double calculateScale = 0.0;
+            Dictionary<int, int> sorted = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, int> item in Main.GetInstance().host.data.GetEmojiResults().OrderByDescending(key => key.Value))
+            {
+                sorted.Add(item.Key, item.Value);
+                count += item.Value;
+            }
+            foreach (int key in sorted.Keys)
+            {
+                resultImages.Add(SurveyManager.GetInstance().GetSurvey().emojis[key].ImageSource);
+            }
+            foreach (int value in sorted.Values)
+            {
+                calculateScale = 3 * (double)value / count;
+                resultScale.Add(calculateScale);
+            }
+            
+            BindingContext = this;
         }
         async void PoistuClicked(object sender, EventArgs e)
         {
