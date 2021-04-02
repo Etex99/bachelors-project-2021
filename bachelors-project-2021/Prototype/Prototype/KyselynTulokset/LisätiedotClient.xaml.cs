9f8339ea747f7ba77211f9b88026e789a9f4e5a5
JesseHuttunen <jesse.huttunen@gmail.com>
2021-04-02 13:22:28 +0300

    Added logic to how bars in bar graph scale depending on the emojiResult results amount

--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/LisätiedotClient.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/LisätiedotClient.xaml.cs
@@ -12,9 +12,36 @@ namespace Prototype
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LisätiedotClient : ContentPage
     {
+        public IList<string> resultImages { get; set; }
+        public IList<double> resultScale { get; set; }
+        public IList<int> resultAmount { get; set; }
         public LisätiedotClient()
         {
             InitializeComponent();
+            resultImages = new List<string>();
+            resultScale = new List<double>();
+            resultAmount = new List<int>();
+
+            int count = 0;
+            double calculateScale = 0.0;
+            Dictionary<int, int> sorted = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, int> item in Main.GetInstance().host.data.GetEmojiResults().OrderByDescending(key => key.Value))
+            {
+                sorted.Add(item.Key, item.Value);
+                resultAmount.Add(item.Value);
+                count += item.Value;
+            }
+            foreach (int key in sorted.Keys)
+            {
+                resultImages.Add(SurveyManager.GetInstance().GetSurvey().emojis[key].ImageSource);
+            }
+            foreach (int value in sorted.Values)
+            {
+                calculateScale = 3 * (double)value / count;
+                resultScale.Add(calculateScale);
+            }
+
+            BindingContext = this;
         }
         async void PoistuClicked(object sender, EventArgs e)
         {
