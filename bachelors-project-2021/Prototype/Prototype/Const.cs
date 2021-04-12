5fd1b0345d0d97c8597b7b8957b99211359ddf00
JesseHuttunen <jesse.huttunen@gmail.com>
2021-04-12 21:20:45 +0300

    Muutettu const valuet dokumentin mukaan

--- bachelors-project-2021/Prototype/Prototype/Const.cs
+++ bachelors-project-2021/Prototype/Prototype/Const.cs
@@ -10,62 +10,44 @@ namespace Prototype
 		public static int vote2Time = 20;
 
 		public static List<string> intros = new List<string>() {
-			"intro1",
-			"intro2",
-			"intro3",
-			"intro4",
-			"intro5"
+			"Tänään minusta tuntuu",
+			"Minun päiväni on ollut",
+			"Tämä tuntui mielestäni",
+			"Mitä mieltä olit tunnista?",
+			"Tämä tunti oli mielestäni"
 		};
 
 		public static Dictionary<int, List<string>> activities = new Dictionary<int, List<string>>() {
 			{ 0, new List<string>() {
-				"emoji 0 activity 1",
-				"emoji 0 activity 2",
-				"emoji 0 activity 3",
-				"emoji 0 activity 4",
-				"emoji 0 activity 5"
+				"Jokainen kertoo mikä oli kivaa",
+				"Kehu vieressä istuvaa",
 			}},
 			{ 1, new List<string>() {
-				"emoji 1 activity 1",
-				"emoji 1 activity 2",
-				"emoji 1 activity 3",
-				"emoji 1 activity 4",
-				"emoji 1 activity 5"
+				"Kerrotaan ohjaajalle mikä hämmästyttää",
+				"Kerrotaan ryhmälle mikä hämmästyttää",
 			}},
 			{ 2, new List<string>() {
-				"emoji 2 activity 1",
-				"emoji 2 activity 2",
-				"emoji 2 activity 3",
-				"emoji 2 activity 4",
-				"emoji 2 activity 5"
+				"Jokainen sanoo jonkin iloisen asian",
+				"Jokainen kertoo ohjaajalle yhden mietteen",
+				"Jokainen kertoo ryhmälle yhden mietteen",
 			}},
 			{ 3, new List<string>() {
-				"emoji 3 activity 1",
-				"emoji 3 activity 2",
-				"emoji 3 activity 3",
-				"emoji 3 activity 4",
-				"emoji 3 activity 5"
+				"Positiivinen palloleikki",
+				"Kirjoitetaan ohjaajalle salattu lappu",
 			}},
 			{ 4, new List<string>() {
-				"emoji 4 activity 1",
-				"emoji 4 activity 2",
-				"emoji 4 activity 3",
-				"emoji 4 activity 4",
-				"emoji 4 activity 5"
+				"5 minuutin tauko",
+				"Siirretään oppitunti ulos",
 			}},
 			{ 5, new List<string>() {
-				"emoji 5 activity 1",
-				"emoji 5 activity 2",
-				"emoji 5 activity 3",
-				"emoji 5 activity 4",
-				"emoji 5 activity 5"
+				"Jokainen kertoo yhden asia mikä mietityttää",
+				"Jokainen kysyy kysymyksen ohjaajalta",
+				"Jokainen kysyy kysymyksen ryhmältä",
 			}},
 			{ 6, new List<string>() {
-				"emoji 6 activity 1",
-				"emoji 6 activity 2",
-				"emoji 6 activity 3",
-				"emoji 6 activity 4",
-				"emoji 6 activity 5"
+				"Matkustetaan kuuhun",
+				"Katsotaan video",
+				"Kerrotaan vitsi",
 			}}
 		};
 	
