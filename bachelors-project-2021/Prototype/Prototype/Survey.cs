3bf846dd9dd316b5a13d12dda2e6d485c316283f
Etex99 <emma99.kem@gmail.com>
2021-04-12 21:19:27 +0300

    Updated Default survey

--- bachelors-project-2021/Prototype/Prototype/Survey.cs
+++ bachelors-project-2021/Prototype/Prototype/Survey.cs
@@ -16,32 +16,32 @@ namespace Prototype
 			List<string> activities;
 
 			Const.activities.TryGetValue(0, out activities);
-			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(0, "Mood 0", "positive", activities, "emoji0.png"));
+			activities = activities.GetRange(0, 2);
+			emojis.Add(new Emoji(0, "Iloinen", "positive", activities, "emoji0.png"));
 
 			Const.activities.TryGetValue(1, out activities);
-			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(1, "Mood 1", "positive", activities, "emoji1.png"));
+			activities = activities.GetRange(0, 2);
+			emojis.Add(new Emoji(1, "Hämmästynyt", "negative", activities, "emoji1.png"));
 
 			Const.activities.TryGetValue(2, out activities);
 			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(2, "Mood 2", "neutral", activities, "emoji2.png"));
+			emojis.Add(new Emoji(2, "Neutraali", "neutral", activities, "emoji2.png"));
 
 			Const.activities.TryGetValue(3, out activities);
-			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(3, "Mood 3", "negative", activities, "emoji3.png"));
+			activities = activities.GetRange(0, 2);
+			emojis.Add(new Emoji(3, "Vihainen", "negative", activities, "emoji3.png"));
 
 			Const.activities.TryGetValue(4, out activities);
-			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(4, "Mood 4", "neutral", activities, "emoji4.png"));
+			activities = activities.GetRange(0, 2);
+			emojis.Add(new Emoji(4, "Väsynyt", "neutral", activities, "emoji4.png"));
 
 			Const.activities.TryGetValue(5, out activities);
 			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(5, "Mood 5", "neutral", activities, "emoji5.png"));
+			emojis.Add(new Emoji(5, "Miettivä", "neutral", activities, "emoji5.png"));
 
 			Const.activities.TryGetValue(6, out activities);
 			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(6, "Mood 6", "positive", activities, "emoji6.png"));
+			emojis.Add(new Emoji(6, "Itkunauru", "positive", activities, "emoji6.png"));
 
 			RoomCode = "1234";
 		}
