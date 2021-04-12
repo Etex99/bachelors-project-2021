8fa3c178b17950e99c5da56a7bb788a512540798
JesseHuttunen <jesse.huttunen@gmail.com>
2021-04-12 17:49:55 +0300

    implemented the second activity vote how it maybe probably should go

--- bachelors-project-2021/Prototype/Prototype/AktiviteettiÄänestys/AktiviteettiäänestysToka.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/AktiviteettiÄänestys/AktiviteettiäänestysToka.xaml.cs
@@ -13,44 +13,18 @@ namespace Prototype
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AktiviteettiäänestysToka : ContentPage
     {
-        // Copy paste go brrrrrrrR
-
         // pitää muuttaa siten, että drop down menu/popup pääsee käsiksi aktiviteetteihin
         //Tätä pitää muuttaa siten, että äänestyksessä mukana vain äänestyksen ensimmäisessä vaiheessa valitut aktiviteetit
 
-        public IList<CollectionItem> Items { get; set; }
-        public class CollectionItem
-        {   
-            //Täällä ei tarvita emojeja
-           // public Emoji Emoji { get; set; }
-            public IList<string> ActivityChoises { get; set; }
-            public ObservableCollection<object> Selected { get; set; }
-
-            public CollectionItem(Emoji emoji, IList<string> activities)
-            {
-            //    Emoji = emoji;
-                ActivityChoises = activities;
-
-                Selected = new ObservableCollection<object>();
-                foreach (var item in emoji.activities)
-                {
-                    Selected.Add(ActivityChoises[ActivityChoises.IndexOf(item)]);
-                }
-            }
-        }
+        public IList<string> Items { get; set; }
+        public string Selected { get; set; } = null;
         public AktiviteettiäänestysToka()
         {
             InitializeComponent();
             NavigationPage.SetHasBackButton(this, false);
 
-            // Copy paste go brrrrrrrRRRRR
-
             //alustus
-            Items = new List<CollectionItem>();
-            foreach (var item in SurveyManager.GetInstance().GetSurvey().emojis)
-            {
-                Items.Add(new CollectionItem(item, Const.activities[item.ID]));
-            }
+            Items = Main.GetInstance().client.voteCandidates2;
 
             BindingContext = this;
         }
@@ -62,28 +36,6 @@ namespace Prototype
             return true;
 
         }
-
-        private void btnPopupButton_Clicked(object sender, EventArgs e)
-        {
-            if (popupSelection.IsVisible == false)
-            {
-
-                popupSelection.IsVisible = true;
-            }
-
-            else if (popupSelection.IsVisible == true)
-            {
-
-                popupSelection.IsVisible = false;
-            }
-
-
-        }
-
-    
-
-
-      
 
         async void JatkaButtonClicked(object sender, EventArgs e)
         {
