930af36314cadd434d04066b7437c948a1191975
Etex99 <emma99.kem@gmail.com>
2021-04-13 12:26:36 +0300

    Actually fixed NaN

--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenVetoHost.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenVetoHost.xaml.cs
@@ -22,22 +22,28 @@ namespace Prototype
 
             int count = 0;
             double calculateScale = 0.0;
-            Dictionary<int, int> sorted = new Dictionary<int, int>();
-            foreach (KeyValuePair<int, int> item in Main.GetInstance().host.data.GetEmojiResults().OrderByDescending(key => key.Value))
-            {
-                sorted.Add(item.Key, item.Value);
-                count += item.Value;
-            }
+            Dictionary<int, int> sorted = new Dictionary<int, int>(Main.GetInstance().host.data.GetEmojiResults().OrderByDescending(key => key.Value) as Dictionary<int, int>);
+            count = sorted.Count;
+
             foreach (int key in sorted.Keys)
             {
                 resultImages.Add(SurveyManager.GetInstance().GetSurvey().emojis[key].ImageSource);
             }
-            foreach (int value in sorted.Values)
-            {
-                calculateScale = 3 * (double)value / count;
-                resultScale.Add(calculateScale);
-            }
 
+            if (count == 0)
+            {
+                foreach (int value in sorted.Values)
+                {
+                    resultScale.Add(0);
+                }
+            } else 
+            {
+                foreach (int value in sorted.Values)
+                {
+                    calculateScale = 3 * (double)value / count;
+                    resultScale.Add(calculateScale);
+                }
+            }
             BindingContext = this;
         }
         async void LopetaClicked(object sender, EventArgs e)
--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenvetoClient.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/YhteenvetoClient.xaml.cs
@@ -22,22 +22,30 @@ namespace Prototype
 
             int count = 0;
             double calculateScale = 0.0;
-            Dictionary<int, int> sorted = new Dictionary<int, int>();
-            foreach (KeyValuePair<int, int> item in Main.GetInstance().client.summary.GetEmojiResults().OrderByDescending(key => key.Value))
-            {
-                sorted.Add(item.Key, item.Value);
-                count += item.Value;
-            }
+            Dictionary<int, int> sorted = new Dictionary<int, int>(Main.GetInstance().host.data.GetEmojiResults().OrderByDescending(key => key.Value) as Dictionary<int, int>);
+            count = sorted.Count;
+
             foreach (int key in sorted.Keys)
             {
                 resultImages.Add(Main.GetInstance().client.survey.emojis[key].ImageSource);
             }
-            foreach (int value in sorted.Values)
+
+            if (count == 0)
             {
-                calculateScale = 3 * (double)value / count;
-                resultScale.Add(calculateScale);
+                foreach (int value in sorted.Values)
+                {
+                    resultScale.Add(0);
+                }
             }
-            
+            else
+            {
+                foreach (int value in sorted.Values)
+                {
+                    calculateScale = 3 * (double)value / count;
+                    resultScale.Add(calculateScale);
+                }
+            }
+
             BindingContext = this;
         }
         async void PoistuClicked(object sender, EventArgs e)
