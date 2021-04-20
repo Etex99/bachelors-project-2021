979922a7bdb0a648ea5a58fc47acf7c7f50b1ab7
Etex99 <emma99.kem@gmail.com>
2021-04-20 20:05:45 +0300

    client does not send empty vote 1 results

--- bachelors-project-2021/Prototype/Prototype/AktiviteettiÄänestys/AktiviteettiäänestysEka.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/AktiviteettiÄänestys/AktiviteettiäänestysEka.xaml.cs
@@ -10,143 +10,141 @@ using Xamarin.Forms.Xaml;
 
 namespace Prototype
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AktiviteettiäänestysEka : ContentPage
-    {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AktiviteettiäänestysEka : ContentPage
+	{
 
-        private int _countSeconds = 10;
+		private int _countSeconds = 10;
 
-        // Copy paste go brrrrrrrR
+		// Copy paste go brrrrrrrR
 
-        public IList<CollectionItem> Items { get; set; }
-        public class CollectionItem
-        {
-            public Emoji Emoji { get; set; }
-            public IList<string> ActivityChoises { get; set; }
-            public string Selected { get; set; } = null;
-
-  
-
-            public CollectionItem(Emoji emoji, IList<string> activities)
-            {
-                Emoji = emoji;
-                ActivityChoises = activities;
-
-                foreach (var item in emoji.activities)
-                {
-                    Console.WriteLine("Item: {0}", item);
-                }
+		public IList<CollectionItem> Items { get; set; }
+		public class CollectionItem
+		{
+			public Emoji Emoji { get; set; }
+			public IList<string> ActivityChoises { get; set; }
+			public string Selected { get; set; } = null;
 
 
-            }
-        }
 
-        public AktiviteettiäänestysEka()
-        {
-            NavigationPage.SetHasBackButton(this, false);
-            InitializeComponent();
-            //alustus
-            List<Emoji> Emojis = Main.GetInstance().client.survey.emojis;
-            Items = new List<CollectionItem>();
+			public CollectionItem(Emoji emoji, IList<string> activities)
+			{
+				Emoji = emoji;
+				ActivityChoises = activities;
 
-          foreach (var item in Main.GetInstance().client.voteCandidates1)
-            {
-                Console.WriteLine("Key: {0}, Value: {1}", item.Key, Emojis[item.Key].activities);
-                Items.Add(new CollectionItem(Emojis[item.Key], Emojis[item.Key].activities));
-
-               
-            } 
-      
-            BindingContext = this;
-
-            Vote1();
-        }
-        
-        private async void Vote1() {
-            
-              _countSeconds = Main.GetInstance().client.vote1Time;
-             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
-            {
-                _countSeconds--;
-
-                timer.Text = _countSeconds.ToString();
+				foreach (var item in emoji.activities)
+				{
+					Console.WriteLine("Item: {0}", item);
+				}
 
 
-                if (_countSeconds == 0)
-                {
-                   
-                   return false;
-  
-                }
+			}
+		}
 
-                return Convert.ToBoolean(_countSeconds);
-            });
+		public AktiviteettiäänestysEka()
+		{
+			NavigationPage.SetHasBackButton(this, false);
+			InitializeComponent();
+			//alustus
+			List<Emoji> Emojis = Main.GetInstance().client.survey.emojis;
+			Items = new List<CollectionItem>();
 
-            await Task.Delay(Main.GetInstance().client.vote1Time * 1000);
-
-            FinishVote1();
-            bool success = await Main.GetInstance().client.ReceiveVote2Candidates();
-            if (success)
-            {
-                //received vote 2 changing view
-                await Navigation.PushAsync(new AktiviteettiäänestysToka());
-            } 
-        }
-
-        //Device back button disabled
-        protected override bool OnBackButtonPressed()
-        {
-            return true;
-
-        }
+			foreach (var item in Main.GetInstance().client.voteCandidates1)
+			{
+				Console.WriteLine("Key: {0}, Value: {1}", item.Key, Emojis[item.Key].activities);
+				Items.Add(new CollectionItem(Emojis[item.Key], Emojis[item.Key].activities));
 
 
-        void btnPopupButton_Clicked(object sender, EventArgs e)
-        {
-            // Edelleen :DDD
-            if (sender is Button b && b.Parent is Grid g && g.Children[2] is Frame f)
-            {
+			}
+
+			BindingContext = this;
+
+			Vote1();
+		}
+
+		private async void Vote1()
+		{
+
+			_countSeconds = Main.GetInstance().client.vote1Time;
+			Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+		   {
+			   _countSeconds--;
+
+			   timer.Text = _countSeconds.ToString();
 
 
-                if (f.IsVisible == false)
-                {
+			   if (_countSeconds == 0)
+			   {
 
-                    f.IsVisible = true;
-                    
-                }
+				   return false;
 
-                else if (f.IsVisible == true)
-                {
+			   }
 
-                    f.IsVisible = false;
-                }
+			   return Convert.ToBoolean(_countSeconds);
+		   });
 
-            }
-        }
+			await Task.Delay(Main.GetInstance().client.vote1Time * 1000);
 
-    
+			SendVote1();
+			bool success = await Main.GetInstance().client.ReceiveVote2Candidates();
+			if (success)
+			{
+				//received vote 2 changing view
+				await Navigation.PushAsync(new AktiviteettiäänestysToka());
+			}
+		}
 
+		//Device back button disabled
+		protected override bool OnBackButtonPressed()
+		{
+			return true;
 
-       async void FinishVote1()
-        {
-            //Copy and Paste
-            //asetetaan emojit survey olioon
-            Dictionary<int, string> answer = new Dictionary<int, string>();
-            foreach (var item in Items)
-            {
-                if(item == null)
-                {
-                    break;
-                }
-                answer.Add(item.Emoji.ID, item.Selected);
-
-                
-            }
-          await Main.GetInstance().client.SendVote1Result(answer);
-
-        
+		}
 
 
-        } 
-    }
+		void btnPopupButton_Clicked(object sender, EventArgs e)
+		{
+			// Edelleen :DDD
+			if (sender is Button b && b.Parent is Grid g && g.Children[2] is Frame f)
+			{
+
+
+				if (f.IsVisible == false)
+				{
+
+					f.IsVisible = true;
+
+				}
+
+				else if (f.IsVisible == true)
+				{
+
+					f.IsVisible = false;
+				}
+
+			}
+		}
+
+
+
+
+		async void SendVote1()
+		{
+			//Copy and Paste
+			//asetetaan emojit survey olioon
+			Dictionary<int, string> answer = new Dictionary<int, string>();
+			foreach (var item in Items)
+			{
+				if (item.Selected == null)
+				{
+					break;
+				}
+				answer.Add(item.Emoji.ID, item.Selected);
+			}
+			if (answer.Count != 0)
+			{
+				await Main.GetInstance().client.SendVote1Result(answer);
+			}
+		}
+	}
 }
