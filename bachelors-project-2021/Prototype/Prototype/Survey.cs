e1071849537a03ce0215504bc811351933b63ddb
Etex99 <emma99.kem@gmail.com>
2021-04-25 16:27:15 +0300

    Create new survey starts with a blank survey

--- bachelors-project-2021/Prototype/Prototype/Survey.cs
+++ bachelors-project-2021/Prototype/Prototype/Survey.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace Prototype
 {
@@ -8,44 +9,82 @@ namespace Prototype
 		public List<Emoji> emojis { get; set; }
 		public string RoomCode { get; set; } = "default";
 		
-		//default survey consists of first intro entry, 7 emojis with various impact each with 3 first entries of activities.
+		//empty constructor creates blank survey
 		public Survey() {
-			introMessage = Const.intros[0];
+			introMessage = null;
 			emojis = new List<Emoji>();
+
+			emojis.Add(new Emoji(0, "Iloinen", "neutral", new List<string>(), "emoji0.png"));
+			emojis.Add(new Emoji(1, "Hämmästynyt", "neutral", new List<string>(), "emoji1.png"));
+			emojis.Add(new Emoji(2, "Neutraali", "neutral", new List<string>(), "emoji2.png"));
+			emojis.Add(new Emoji(3, "Vihainen", "neutral", new List<string>(), "emoji3.png"));
+			emojis.Add(new Emoji(4, "Väsynyt", "neutral", new List<string>(), "emoji4.png"));
+			emojis.Add(new Emoji(5, "Miettivä", "neutral", new List<string>(), "emoji5.png"));
+			emojis.Add(new Emoji(6, "Itkunauru", "neutral", new List<string>(), "emoji6.png"));
+
+			RoomCode = null;
+		}
+		public Survey(string introMessage, List<Emoji> emojis, string RoomCode)
+		{
+			this.introMessage = introMessage;
+			this.emojis = emojis;
+			this.RoomCode = RoomCode;
+		}
+
+		//default survey consists of first intro entry, 7 emojis with various impact each with 3 first entries of activities and a random roomcode
+		public Survey GetDefaultSurvey()
+		{
+			string tempIntro = Const.intros[0];
+			List<Emoji> tempEmojis = new List<Emoji>();
 
 			List<string> activities;
 
 			Const.activities.TryGetValue(0, out activities);
 			activities = activities.GetRange(0, 2);
-			emojis.Add(new Emoji(0, "Iloinen", "positive", activities, "emoji0.png"));
+			tempEmojis.Add(new Emoji(0, "Iloinen", "positive", activities, "emoji0.png"));
 
 			Const.activities.TryGetValue(1, out activities);
 			activities = activities.GetRange(0, 2);
-			emojis.Add(new Emoji(1, "Hämmästynyt", "negative", activities, "emoji1.png"));
+			tempEmojis.Add(new Emoji(1, "Hämmästynyt", "negative", activities, "emoji1.png"));
 
 			Const.activities.TryGetValue(2, out activities);
 			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(2, "Neutraali", "neutral", activities, "emoji2.png"));
+			tempEmojis.Add(new Emoji(2, "Neutraali", "neutral", activities, "emoji2.png"));
 
 			Const.activities.TryGetValue(3, out activities);
 			activities = activities.GetRange(0, 2);
-			emojis.Add(new Emoji(3, "Vihainen", "negative", activities, "emoji3.png"));
+			tempEmojis.Add(new Emoji(3, "Vihainen", "negative", activities, "emoji3.png"));
 
 			Const.activities.TryGetValue(4, out activities);
 			activities = activities.GetRange(0, 2);
-			emojis.Add(new Emoji(4, "Väsynyt", "neutral", activities, "emoji4.png"));
+			tempEmojis.Add(new Emoji(4, "Väsynyt", "neutral", activities, "emoji4.png"));
 
 			Const.activities.TryGetValue(5, out activities);
 			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(5, "Miettivä", "neutral", activities, "emoji5.png"));
+			tempEmojis.Add(new Emoji(5, "Miettivä", "neutral", activities, "emoji5.png"));
 
 			Const.activities.TryGetValue(6, out activities);
 			activities = activities.GetRange(0, 3);
-			emojis.Add(new Emoji(6, "Itkunauru", "positive", activities, "emoji6.png"));
+			tempEmojis.Add(new Emoji(6, "Itkunauru", "positive", activities, "emoji6.png"));
 
-			RoomCode = "1234";
+			string TempRoomCode = GenerateRandomCode();
+
+			return new Survey(tempIntro, tempEmojis, TempRoomCode);
 		}
 
+		//generates random room code of 5 numbers
+		private string GenerateRandomCode()
+		{
+			Random r = new Random();
+			string temp = "";
+
+			for (int i = 0; i < 5; i++)
+			{
+				temp += r.Next(10).ToString();
+			}
+
+			return temp;
+		}
 		//toString method for getting the info of the survey in a string for data purposes
 		public override string ToString() {
 			string value = "";
