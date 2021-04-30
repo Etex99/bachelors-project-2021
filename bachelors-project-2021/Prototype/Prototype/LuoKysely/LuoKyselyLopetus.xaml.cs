e007637eb8390e74deed9efd3bd105caeb9c2c6c
JesseHuttunen <jesse.huttunen@gmail.com>
2021-04-30 16:27:06 +0300

    added TextChanged part in both entries so it only allows numbers and letters

--- bachelors-project-2021/Prototype/Prototype/LuoKysely/LuoKyselyLopetus.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/LuoKysely/LuoKyselyLopetus.xaml.cs
@@ -109,5 +109,14 @@ namespace Prototype
             // siirrytään OdotettaanVastauksia sivulle 
             await Navigation.PushAsync(new OdotetaanVastauksia());
         }
+        private static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                bool isValid = args.NewTextValue.ToCharArray().All(x => char.IsLetterOrDigit(x));
+
+                ((Entry)sender).Text = isValid ? args.NewTextValue : args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+            }
+        }
     }
 }
